962950244475f28b984dea464b9054fa0ca5b634
Bagas Maulana <bagas.maulanarela@gmail.com>
2024-10-12 12:04:42 +0700

    footer tok

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="footer bg-light">
+                  <div
+                    class="container-fluid d-flex flex-md-row flex-column justify-content-between align-items-md-center gap-1 container-p-x py-3"
+                  >
+                    <div>
+                      <a
+                        href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/landing/"
+                        target="_blank"
+                        class="footer-text fw-bolder"
+                        >GoArca</a
+                      >
+                      ©
+                    </div>
+                    <div>
+                      <a href="javascript:void(0)" class="btn btn-sm btn-outline-danger"
+                        ><i class="bx bx-log-out-circle"></i> Logout</a
+                      >
+                    </div>
+                  </div>
+                </footer>
