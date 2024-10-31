90f1f046100969c55f34175ccf6d8a1bbb30d16a
Bagas Maulana <bagas.maulanarela@gmail.com>
2024-10-31 11:35:02 +0700

    Fix Worker

--- app/Console/Commands/UpdateRetentionStatus.php
+++ app/Console/Commands/UpdateRetentionStatus.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\DaftarArsip;
+use App\Models\Log;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
@@ -28,21 +29,35 @@ class UpdateRetentionStatus extends Command
      */
     public function handle()
     {
-        // Ambil tanggal hari ini
         $today = Carbon::now()->toDateString();
     
         // Ambil semua data yang dibutuhkan dari tabel daftar_arsips
-        $arsips = DB::table('daftar_arsips')->get(['id', 'tahun_berkas', 'jumlah_retensi']);
+        $arsips = DB::table('daftar_arsips')->get(['id', 'tahun_berkas', 'jumlah_retensi', 'nasib']);
     
         foreach ($arsips as $arsip) {
             // Hitung tahun_musnah (tahun_berkas + jumlah_retensi)
             $tahun_musnah = Carbon::createFromDate($arsip->tahun_berkas)->addYears($arsip->jumlah_retensi)->toDateString();
     
-            // Jika tahun_musnah lebih kecil dari hari ini, update status 'nasib' menjadi 'Musnah'
-            if ($tahun_musnah < $today) {
+            // Jika tahun_musnah lebih kecil dari hari ini, update status 'nasib' menjadi 'Permanen'
+            if ($tahun_musnah > $today) {
+                // Ambil data lama sebelum diupdate
+                $oldData = [
+                    'nasib' => $arsip->nasib,
+                ];
+
+                // Update status 'nasib' menjadi 'Permanen'
                 DB::table('daftar_arsips')
                     ->where('id', $arsip->id)
-                    ->update(['nasib' => 'Permanen']);
+                    ->update(['nasib' => 'Musnah']);
+
+                // Simpan log perubahan
+                Log::create([
+                    'action' => 'updated',
+                    'table_name' => 'daftar_arsips',
+                    'record_id' => $arsip->id,
+                    'old_data' => json_encode($oldData), // Data sebelum perubahan
+                    'new_data' => json_encode(['nasib' => 'Musnah']), // Data sesudah perubahan
+                ]);
             }
         }
     
