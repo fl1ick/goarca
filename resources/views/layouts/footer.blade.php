66c1550ec09d663be7b3c39303adb8cc5356c122
Slametainul21 <slametainul09@gmail.com>
2025-03-04 12:47:00 +0700

    footer fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
-<!-- Footer -->
-<footer class="footer bg-dark">
-      <div class="footer-text">
-        &copy; 2024 GoArca. Hak Cipta Dilindungi Undang-Undang Pemerintah Kota Magelang
+<footer id="footer">
+  <div class="footer-container">
+      <div class="footer-center">
+          <p>&copy; 2025 Dinas Perpustakaan dan Kearsipan Kota Magelang . Semua Hak Dilindungi.</p>
       </div>
+  </div>
 </footer>
